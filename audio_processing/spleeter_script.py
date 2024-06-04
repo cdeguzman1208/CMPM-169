e834a825ab4e734b0727f2307a6ec9edf90aa3d2
Cromwell De Guzman <cdeguzman1208@gmail.com>
2024-06-03 18:14:26 -0700

    i give up

--- audio_processing/spleeter_script.py
+++
@@ -1,7 +0,0 @@
-from spleeter.separator import Separator
-
-# Initialize Spleeter with 2 stems (vocals and accompaniment)
-separator = Separator('spleeter:2stems')
-
-# Separate the audio into vocal and instrumental components
-separator.separate_to_file('input_song.mp3', 'output')
--- check_ffmpeg.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-try:
-    result = subprocess.run(['ffmpeg', '-version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    print(result.stdout.decode())
-except FileNotFoundError:
-    print("FFmpeg not found")
